d62fc83a49392dd14ff84445f4ceb1f31bc2b266
Reggis94 <regisboamah@gmail.com>
2020-06-29 03:50:25 +0200

    Simple login page

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = new User();
+        // ...
+        $user->setEmail("admin@dricub.com");
+        $user->setRoles(["ROLE_ADMIN"]);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'dricub'
+        ));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+}
